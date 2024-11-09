9ebaf629320c77d756aa42d3cda07fba76ecbebc
Tiankuo Zhang <2849227721@qq.com>
2024-11-09 23:31:16 +0800

    Add files via upload

---
+++ Prime_Sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ int main ()
+{
+int a,b,count=0,cnt=0,sum=0;
+scanf("%d %d",&a,&b);
+int i=a ;
+for(i; i<b; i++){//go through a to b
+    cnt=0;// renew cnt
+    for(int check=2; check<i; check++){
+        if(i%check==0 && i!=2) {
+            cnt=0;
+            break;
+        }
+        else cnt++;
+    }//this loop to check prime number
+    if(cnt) {
+        sum=sum+i;
+        count++;
+    }
+ }
+ printf("%d,%d",count,sum);
+}
