11ce16bae53a6483bb6ef91c81e6f7a65354ba90
Lazy Sheep <2849227721@qq.com>
2024-11-10 00:15:01 +0800

    Update Prime_Sum.c

--- Prime_Sum.c
+++ Prime_Sum.c
@@ -4,10 +4,10 @@
 int a,b,count=0,cnt=0,sum=0;
 scanf("%d %d",&a,&b);
 int i=a ;
-for(i; i<b; i++){//go through a to b
+for(i; i<=b; i++){//go through a to b
     cnt=0;// renew cnt
-    for(int check=2; check<i; check++){
-        if(i%check==0 && i!=2) {
+    for(int check=2; check<=i; check++){
+        if(i%check==0 && i!=check) {
             cnt=0;
             break;
         }
@@ -16,7 +16,9 @@ for(i; i<b; i++){//go through a to b
     if(cnt) {
         sum=sum+i;
         count++;
+    printf("%d ",i);
     }
  }
  printf("%d,%d",count,sum);
+ return 0;
 }
